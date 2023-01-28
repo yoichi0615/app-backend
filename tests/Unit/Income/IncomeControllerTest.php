fefab2893b3be85258cc48e00a486ef5a4b1cd21
yoichi0615 <aaa11655pt@gmail.com>
2023-01-28 20:23:12 +0900

    テストコード修正

--- tests/Unit/Income/IncomeControllerTest.php
+++ tests/Unit/Income/IncomeControllerTest.php
@@ -31,15 +31,26 @@ class IncomeControllerTest extends TestCase
      */
     public function testGetMonthlyData($request)
     {
-        Collection::times(10, function() {
+        Collection::times(15, function () {
             Income::factory()->create([
-                'date' => $this->faker->dateTimeBetween($startDate = '-2 week', $endDate = '+2 week'),
+                'date' => $this->faker->dateTimeBetween($startDate = '2022-12-01', $endDate = '2023-06-11'),
             ]);
         });
 
-        $now = Carbon::now();
+        $request = isset($request['date']) ? $request : null;
+        if ($request) {
+            $targetDate = Carbon::create($request['date']);
+            $targetStartDate = $targetDate->startOfMonth()->startOfDay()->format('Y-m-d');
+            $targetEndDate = $targetDate->endOfMonth()->format('Y-m-d');
+        } else {
+            $now = Carbon::now();
+            $targetStartDate = $now->startOfMonth()->startOfDay()->format('Y-m-d');
+            $targetEndDate = $now->endOfMonth()->endOfDay()->format('Y-m-d');
+        }
+
         $response = $this->get(route('get', $request));
-        $expected = Income::whereBetween('date', [$now->startOfMonth()->startOfDay()->format('Y-m-d'), $now->endOfMonth()->endOfDay()->format('Y-m-d')])->get();
+        $expected = Income::whereBetween('date', [$targetStartDate, $targetEndDate])->get();
+
         $response->assertStatus(200)
             ->assertExactJson($expected->toArray());
     }
@@ -47,15 +58,22 @@ class IncomeControllerTest extends TestCase
 
     public function provideAdditionTestParams()
     {
-        $now = Carbon::now();
         return [
-            //テスト1週目
             [
                 [
-                    'date' => $now->format('Y-m-d')
-                ],
+                    'date' => Carbon::parse('2022-11-11')->format('Y-m-d')
+                ]
             ],
-            //テスト2週目
+            [
+                [
+                    'date' => Carbon::parse('2023-03-11')->format('Y-m-d')
+                ]
+            ],
+            [
+                [
+                    'date' => Carbon::parse('2021-11-12')->format('Y-m-d')
+                ]
+            ],
             [
                 [],
             ],
