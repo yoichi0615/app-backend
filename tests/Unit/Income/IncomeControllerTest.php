ab992f121bbb02fccd5621f85de292e552d4d3fe
yoichi0615 <aaa11655pt@gmail.com>
2023-05-10 17:45:53 +0900

    修正

--- tests/Unit/Income/IncomeControllerTest.php
+++ tests/Unit/Income/IncomeControllerTest.php
@@ -33,11 +33,11 @@ class IncomeControllerTest extends TestCase
      */
     public function testGetMonthlyData($request)
     {
-        // Collection::times(15, function () {
+        Collection::times(15, function () {
             Income::factory(5)->create([
                 'date' => $this->faker->dateTimeBetween($startDate = '2022-12-01', $endDate = '2023-06-11'),
             ]);
-        // });
+        });
 
         $request = isset($request['date']) ? $request : null;
         if ($request) {
